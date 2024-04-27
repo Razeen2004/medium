923404d110ec50b45a75e5ad588f142e06b88b09
Razeen <razeenbaig10@gmail.com>
2024-04-27 11:38:47 +0500

    changes made for this, field added

---
+++ frontend/src/components/Feed.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Feed = () => {
+  return (
+    <div className="feed">
+        <div className="container">
+            <div className="left-sec">
+                <div className="cards">
+                    <div className="card">
+                        <div className="card-left-sec">
+                            <div className="card-author">
+                                
+                            </div>
+                            <div className="card-title"></div>
+                            <div className="card-date">
+
+                            </div>
+                        </div>
+                        <div className="card-right-sec">
+                            <img src="" alt="" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="right-sec">
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Feed
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import Trending from '../components/Trending'
+import Feed from '../components/Feed'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
         <Navbar />
         <Hero />
         <Trending/>
+        <Feed />
     </div>
   )
 }
